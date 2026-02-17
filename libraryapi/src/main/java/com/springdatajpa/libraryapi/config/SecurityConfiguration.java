bc84ccb070514cc89d45cdedce14629ec31c481b
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-02-17 12:40:26 -0300

    ✨ feat: Implementa `CustomUserDetailsService` para suporte a autenticação baseada em banco de dados

--- libraryapi/src/main/java/com/springdatajpa/libraryapi/config/SecurityConfiguration.java
+++ libraryapi/src/main/java/com/springdatajpa/libraryapi/config/SecurityConfiguration.java
@@ -1,17 +1,17 @@
 package com.springdatajpa.libraryapi.config;
 
+import com.springdatajpa.libraryapi.security.CustomUserDetailsService;
+import com.springdatajpa.libraryapi.service.UsuarioService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,6 +26,7 @@ public class SecurityConfiguration {
                 .formLogin(configurer -> configurer.loginPage("/login"))
                 .authorizeHttpRequests(authorize -> {
                     authorize.requestMatchers("/login").permitAll();
+                    authorize.requestMatchers(HttpMethod.POST, "/usuarios").permitAll();
                     authorize.requestMatchers("/autores/**").hasRole("ADMIN");
                     authorize.requestMatchers("/livros/**").hasAnyRole("USER", "ADMIN");
 
@@ -40,20 +41,22 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user1 = User.builder()
-                .username("usuario")
-                .password("123")
-                .roles("USER")
-                .build();
-
-        UserDetails user2 = User.builder()
-                .username("admin")
-                .password("1234")
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user1, user2);
+    public UserDetailsService userDetailsService(UsuarioService usuarioService) {
+//        UserDetails user1 = User.builder()
+//                .username("usuario")
+//                .password("123")
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails user2 = User.builder()
+//                .username("admin")
+//                .password("1234")
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user1, user2);
+//    }
+        return new CustomUserDetailsService(usuarioService);
     }
 
 }
