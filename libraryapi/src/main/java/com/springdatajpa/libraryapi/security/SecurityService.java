8d85c5a9fe8fb60479f8c81d1e2deb199397aa2b
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-02-25 12:50:02 -0300

    ✨ feat: Implementa configuração de segurança com Spring Security e serviço para obter usuário logado

---
+++ libraryapi/src/main/java/com/springdatajpa/libraryapi/security/SecurityService.java
@@ -0,0 +1,21 @@
+package com.springdatajpa.libraryapi.security;
+
+import com.springdatajpa.libraryapi.model.Usuario;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityService {
+
+    public Usuario obterUsuarioLogado(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication instanceof CustomAuthentication customAuth){
+            return customAuth.getUsuario();
+
+        }
+        return null;
+    }
+
+}
