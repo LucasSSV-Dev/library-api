a9c41e2118a58169c882af89dd1fea9d629061ed
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2026-02-25 12:49:28 -0300

    ✨ feat: Adiciona LoginViewController com endpoints para página de login e saudação na página inicial

---
+++ libraryapi/src/main/java/com/springdatajpa/libraryapi/controller/LoginViewController.java
@@ -0,0 +1,24 @@
+package com.springdatajpa.libraryapi.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping()
+public class LoginViewController {
+
+    @GetMapping("/login")
+    public String paginaLogin(){
+        return "login";
+    }
+
+    @GetMapping("/")
+    @ResponseBody
+    public String paginaInicial(Authentication authentication){
+        return "Olá, " + authentication.getName() + "!";//!= null ? "index" : "redirect:/login";
+    }
+
+}
